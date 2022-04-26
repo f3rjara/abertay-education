569b5ca0ee3ed04a37f327049119db9ccabdd558
fernando.jaramillo <fernando.jaramillo@branch.com.co>
2022-04-26 16:38:41 -0500

    add component why abertay

--- branch-theme/template-parts/page-builder.php
+++ branch-theme/template-parts/page-builder.php
@@ -12,3 +12,7 @@
   if( get_row_layout() == 'sub_component_hero_banner_text' ):
     get_template_part('template-parts/components/hero_banner_text');
   endif;
+
+  if( get_row_layout() == 'sub_component_why_abertay' ):
+    get_template_part('template-parts/components/why_abertay');
+  endif;
